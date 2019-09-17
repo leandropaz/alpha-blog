2c42999df9a2859ded0f8beae6f7b5e0adb782c3
Leandro Paz <leandropaz@gmail.com>
2019-09-16 22:24:48 -0300

    styled success and error messages.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
         # render plain: params[:article].inspect
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = 'Article created!'
+            flash[:success] = 'Article created!'
             redirect_to article_path(@article)
         else
             render 'new'
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
 
     def update
         if @article.update(article_params)
-            flash[:notice] = 'Article updated successfully'
+            flash[:success] = 'Article updated successfully'
             redirect_to article_path(@article)
         else
             render 'edit'
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
 
     def destroy
         @article.destroy
-        flash[:notice] = 'The article was deleted!'
+        flash[:danger] = 'The article was deleted!'
         redirect_to articles_path
     end
 
